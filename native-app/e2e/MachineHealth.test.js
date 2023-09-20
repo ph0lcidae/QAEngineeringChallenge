bdc21f8a6811093803c65df734adf90aa8b7da87
Alana <firinneceartas@protonmail.com>
2023-09-20 16:51:51 -0700

    feat: base e2e tests now running, added base case

--- native-app/e2e/MachineHealth.test.js
+++ native-app/e2e/MachineHealth.test.js
@@ -1,11 +1,27 @@
 const { reloadApp } = require("detox-expo-helpers");
 
-describe("Example", () => {
+describe("Machine health calculation user flows", () => {
   beforeAll(async () => {
     await device.launchApp();
   });
 
   beforeEach(async () => {
-    await device.reloadApp();
+    await reloadApp();
   });
+
+  afterAll(async () => {
+    await device.terminateApp();
+  });
+
+  it("selects a part from the part picker, enters a valid value, and saves", async () => {
+    await element(by.text('Go to home screen!')).tap();
+    await expect(element(by.text('Calculate Health'))).toBeVisible();
+  });
+
+  it("selects multiple parts with valid values, saves, and calculates machine state", async () => {
+    await element(by.text('Go to home screen!')).tap();
+    await expect(element(by.text('Calculate Health'))).toBeVisible();
+  });
+
+  
 });
