9581d4d8cdf1db0464b0bdf091898efabade6565
Alana <firinneceartas@protonmail.com>
2023-09-20 19:50:37 -0700

    chore: detox helpers and package to fix CORS issue

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import { getMachineHealth } from "./machineHealth";
+import cors from 'cors';
 
 const app = express();
 const port = 3001;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+
+// Fixes CORS issue with backend and native-app
+app.use(cors());
 
 // Endpoint to get machine health score
 app.post("/machine-health", (req: Request, res: Response) => {
